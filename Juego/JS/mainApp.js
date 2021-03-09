4854cc293e884a6e7074c53e43add119b5e10023
gerardobecerra1 <42364794+gerardobecerra1@users.noreply.github.com>
2021-03-09 15:22:33 -0600

    mas modales

--- Juego/JS/mainApp.js
+++ Juego/JS/mainApp.js
@@ -1,19 +1,31 @@
-var beepHover = $(".soundHover")[0];
-
 $(document).ready(function () {
+
+    var beepHover = $(".soundHover")[0];
+    var beepTransition = $(".soundTransition")[0];
+    var beepTranReverse = $(".soundTranReverse")[0];
+
+    $(".navbar").hide();
+
+    $(".btn").hover(function () {
+        beepHover.play();
+    }, function () {});
+
+    $(".btn-menu").click(function (e) {
+        beepTransition.play();
+    });
+
+    $(".btn-reverse").click(function (e) {
+        beepTranReverse.play();
+    });
 
     $("#btnPlay").click(function () {
         $(".menu").remove();
         $(".estrellas").remove();
         $(".brillando").remove();
         $(".nubes").remove();
+        $(".navbar").show();
         IntiGame();
     });
-
-    $(".btn").hover(function () {
-        beepHover.play();
-    }, function () {});
-
 
     function IntiGame() {
         const scene = new THREE.Scene();
@@ -31,6 +43,39 @@ $(document).ready(function () {
         scene.add(cube);
 
         camera.position.z = 5;
+
+        $(document).on('keydown', function (event) {
+            if (event.key == "Escape") {
+                beepTransition.play();
+                modalWrap = document.createElement('div');
+                modalWrap.innerHTML = `
+                    <div class="modal modal-pause fade" id="PauseModal" tabindex="-1" aria-labelledby="PauseModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-header">
+                                <h1 class="modal-title" id="PauseModalLabel">Pause</h1>
+                            </div>
+                            <div class="modal-content">
+                            <div class="modal-body">
+                                <div class="row">
+                                    <button type="button" class="btn btn-online btn-reverse mb-4" data-bs-dismiss="modal">Resume</button>
+                                </div>
+                                <div class="row">
+                                    <button type="button" class="btn btn-menu mb-4" id="btnSettings">Settings</button>
+                                </div>
+                                <div class="row">
+                                <button type="button" class="btn btn-menu mb-4" id="btnLeave">Leave</button>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>`;
+                document.body.append(modalWrap);
+                var modal = new bootstrap.Modal(modalWrap.querySelector('.modal-pause'));
+                modal.show();
+            } else if (modalWrap !== null) {
+                modalWrap.remove();
+            }
+        });
 
         animate();
 
