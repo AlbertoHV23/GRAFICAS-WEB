61f8a3670fa54c461838dc854ecea2ac1c9f59ca
gerardobecerra1 <42364794+gerardobecerra1@users.noreply.github.com>
2021-03-09 18:37:12 -0600

    Todas las pantallas listas

--- Juego/JS/mainApp.js
+++ Juego/JS/mainApp.js
@@ -3,8 +3,19 @@ $(document).ready(function () {
     var beepHover = $(".soundHover")[0];
     var beepTransition = $(".soundTransition")[0];
     var beepTranReverse = $(".soundTranReverse")[0];
+    var sound = true;
 
-    $(".navbar").hide();
+    $(".btnSound").click(function (e) {
+        if (($(this).hasClass("active") === false) && ($(this).hasClass("btnOn") === true)) {
+            $(this).addClass("active");
+            $(".btnOff").removeClass("active");
+            sound = true;
+        } else {
+            $(".btnOn").removeClass("active");
+            $(this).addClass("active");
+            sound = false;
+        }
+    });
 
     $(".btn").hover(function () {
         beepHover.play();
@@ -18,7 +29,8 @@ $(document).ready(function () {
         beepTranReverse.play();
     });
 
-    $("#btnPlay").click(function () {
+
+    $("#btnSolo").click(function () {
         $(".menu").remove();
         $(".estrellas").remove();
         $(".brillando").remove();
@@ -83,6 +95,7 @@ $(document).ready(function () {
             requestAnimationFrame(animate);
             cube.rotation.x += 0.01;
             cube.rotation.y += 0.01;
+            cube.rotation.z += 0.01;
             renderer.render(scene, camera);
         }
     }
